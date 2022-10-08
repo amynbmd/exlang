7ead83e7860b807982914cf658add02e8e264e1d
amynbmd <nguyenbamyduyen1998@gmail.com>
2022-10-08 15:18:46 -0400

    clean up

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/npham'),
+      dir: require('path').join(__dirname, './coverage/exlang'),
       subdir: '.',
       reporters: [
         { type: 'html' },
