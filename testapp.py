5fa4e7643ef4dff7e0e0948086356b2bd463aa00
Tran Tran <trantran@Trans-MacBook-Air.local>
2022-10-17 23:44:31 -0400

    Commit

--- testapp.py
+++ testapp.py
@@ -1,23 +1,29 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_login import LoginManager
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 #function to create web server
 
 #Create an instance of Flask as 'app'
 app = Flask(__name__)
 CORS(app)
+app.config['SECRET_KEY'] = 'exlang'
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://{username}:{password}@{server}/exlang".format(
-username="root", password= "", server="localhost")
+username="root", password= "nhutran2002", server="localhost")
 db = SQLAlchemy(app)
 
 class User(db.Model):
     __tablename__ = 'user'
-    username  = db.Column(db.String(50), primary_key = True)
+    name  = db.Column(db.String(50), primary_key = True)
     email = db.Column(db.String(50), unique = True)
     password = db.Column(db.String(50))
 
+    def __repr__(self):
+        return "name: {0) | email: {1} | password: {2}".format(self.name, self.email, self.password)
 
 @app.route("/")
 def landing_page():
@@ -27,10 +33,27 @@ def landing_page():
 def home_page():
     return "<p>Welcome to our home page!<p>"
 
-# @app.route("/login")
-# def login():
-#     return render_template("index.html")
+#LOGIN
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+    
+    user = User.query.filter_by(email=email).first()
+    user1 = bool(User.query.filter_by(password=password).first())
+    if user:
+        if  user1:
+            flash('Logged in successfully!', category='success')
+            return "<p> Success <p>"
+        else: 
+            flash('Incorrect password, try again.', category='error')
+    else:
+        flash('Incorrect email, try again.', category='error')
 
+    return "<p>Success <p>"
+
+#SIGN-UP
 @app.route("/register", methods=['GET', 'POST'])
 def signup_page():
     if request.method == 'POST':
@@ -39,5 +62,14 @@ def signup_page():
         password = request.form.get("password")
         confirmPassword = request.form.get("confirmPassword")
         
-    return "<p>Sign Up Here!!!<p>"
+        new_user = User(email=email, password=password, name = name)
 
+        db.session.add(new_user)
+        db.session.commit
+        
+        # login_user(new_user, remember=True)
+        # flash('Account created!', category='success')
+        
+        return "<p>Success <p>"
+
+app.run()
